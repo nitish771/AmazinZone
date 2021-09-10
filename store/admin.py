10c0dd1ab621048eb9a422157a509206197cd0f0
Nitish <nkm.081420@gmail.com>
2021-09-10 15:09:20 +0530

    - Add context processor - add prepopulated field for slug

--- store/admin.py
+++ store/admin.py
@@ -3,8 +3,10 @@ from .models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('title', 'category', 'price', 'quantity')
-	list_filter = ('price', 'category')
+    list_display = ('title', 'category', 'price', 'quantity')
+    list_filter = ('price', 'category')
+    # insert slug with entering title
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Product, ProductAdmin)
